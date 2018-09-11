bef351f218833fa3c528fe502c58d6cbd91f5b29
Luke Inglis <linglis@dmgroupk12.com>
2018-09-11 01:17:38 -0400

    Add SimpleCov and configure Travis for its usage
    Allows Travis to report coverage information back to CodeClimate.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'rspec', '~> 3.0.0'
 gem 'fuubar'
+em 'simplecov', require: false, group: :test
--- spec/noop_spec.rb
+++ spec/noop_spec.rb
@@ -1,14 +1,17 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'noop.rb'
 
 describe Noop do
-  context "#noop" do
-    it "returns nothing" do
+  context '#noop' do
+    it 'returns nothing' do
       expect(subject.noop).to be_nil
     end
   end
 
-  context "#roc" do
-    it "returns a proc which returns nothing" do
+  context '#roc' do
+    it 'returns a proc which returns nothing' do
       expect(subject.roc.call).to be_nil
     end
   end
